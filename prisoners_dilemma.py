bcc46df9df17b431f056c9992df7c8e91700c91e
Stewartsean <stewartsean19@gmail.com>
2015-11-11 10:50:05 -0600

    Edits to player 10

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 #Fixing Github
 #one more try
-''' one final change
+''' Sean Stewart
+one final change
 PrisonerDilemma.py allows hard-coding different strategies
 for the Iterative Prisoners Dilemma, the canonical game of game-theory.
 Each strategy plays 100 to 200 rounds against each other strategy.
@@ -371,17 +372,19 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 10:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Jake and Sean'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
-
+            if opponent_history[-1]=='b':
+                 return 'b'    
+            elif history[-1]=='c' and opponent_history[-1]=='c':
+                return 'c' # betray is they were severely punished last time
+                
+        
+            
 
 
 
